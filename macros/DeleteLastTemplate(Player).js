d6fa21baa31b5570bdb8072b422db884d86021ff
Dave <33916281+Belodri@users.noreply.github.com>
2024-04-14 16:08:06 +0200

    Create DeleteLastTemplate(Player).js

---
+++ macros/DeleteLastTemplate(Player).js
@@ -0,0 +1,10 @@
+//Delete the last template the user placed, even if it's not the last template in the array
+for (let i = canvas.templates.placeables.length; i > 0; i--) {
+    try {
+        canvas.templates.placeables.at(i).document.delete();
+        break;
+    }
+    catch {
+        continue
+    }
+}
