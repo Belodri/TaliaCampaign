106b6b33ccfe2233baf9ba48e030d3f7b75654a2
Dave <33916281+Belodri@users.noreply.github.com>
2024-04-30 15:51:36 +0200

    Update markDefeated.js

--- macros/markDefeated.js
+++ macros/markDefeated.js
@@ -1,8 +1,9 @@
-//marks a token as dead & defeated with the same overlay AE used in combat.
+//sets a token's hp to 0 & marks it as dead & defeated with the same overlay AE used in combat.
 
 canvas.tokens.controlled.map(async token => {
     if(token.document.actor.type === "npc") {
         const effect = CONFIG.statusEffects.find(e => e.id === CONFIG.specialStatusEffects.DEFEATED);
+        await token.actor.update({"system.attributes.hp.value": 0});
         await token.document.toggleActiveEffect(effect, {overlay: true});
         if(token.combatant) {
             await token.combatant.update({defeated: true});
